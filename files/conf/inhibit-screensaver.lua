051ee7d5d6c6ffe4907d12067bf43da17e444a2e
Aaron Bugher <aabugher@gmail.com>
2024-02-20 14:24:05 -0500

    This does not yield errors in the log.  The screensaver seems to turn off at playback start.  Screensaver kicks in while playback is paused and goes away when playback resumes.

--- files/conf/inhibit-screensaver.lua
+++ files/conf/inhibit-screensaver.lua
@@ -1,11 +1,38 @@
 -- Comments look like this.
 local utils = require 'mp.utils'
 
-function inhibit()
-    utils.subprocess({env={"DISPLAY=:0", "XAUTHORITY=/home/display/.Xauthority"},args={"xdg-screensaver", "reset"}})
+
+-- Unconditionally reset the screen saver inactivity countdown.
+local function inhibit()
+    utils.subprocess( {
+        env={
+            "DISPLAY=:0", 
+            "XAUTHORITY=/home/display/.Xauthority"
+        }, args={
+            "xdg-screensaver", 
+            "reset"
+        }
+    } )
 end
 
--- Inhibit screensaver every 30 seconds.
-mp.add_periodic_timer(30, inhibit)
--- Don't wait 30 seconds to turn off the screen saver.
-inhibit
+
+-- Conditionally reset the screen saver inactivity countdown.
+local function heartbeat(property_name, property_value)
+    -- While paused or not playing anything, let the screen saver run.
+    if 
+        mp.get_property_bool("pause")
+        or mp.get_property_bool("idle-active")
+    then
+        return
+    end
+
+    -- If actively playing, inhibit the screen saver.
+    return inhibit()
+end
+
+
+-- Establish periodic heartbeat.
+mp.add_periodic_timer(30, heartbeat)
+-- Hastily turn off the screen saver when playback starts.  (Fire but do nothing when playback stops.)
+mp.observe_property("idle-active", "bool", heartbeat)
+mp.observe_property("pause", "bool", heartbeat)
